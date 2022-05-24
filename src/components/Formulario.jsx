b2bcb426f5320d03e346049a83feb023a1f9728c
FherAmableRicse <amablericsefher@gmail.com>
2022-05-24 17:59:19 -0500

    Funcionalidad editar y eliminar

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import Error from './Error';
 
-const Formulario = ({pacientes,setPacientes}) => {
+const Formulario = ({ pacientes, setPacientes, paciente,setPaciente }) => {
 
   const [nombre, setNombre] = useState('');
   const [propietario, setPropietario] = useState('');
@@ -10,7 +10,16 @@ const Formulario = ({pacientes,setPacientes}) => {
   const [sintomas, setSintomas] = useState('');
 
   const [error, setError] = useState(false);
-
+  useEffect(() => {
+    if (Object.keys(paciente).length > 0) {
+      setNombre(paciente.nombre);
+      setPropietario(paciente.propietario);
+      setEmail(paciente.email);
+      setFecha(paciente.fecha);
+      setSintomas(paciente.sintomas);
+    }
+  }, [paciente])
+ 
   const generarId = () => {
     const random = Math.random().toString(36).substr(2);
     const fecha = Date.now().toString(36);
@@ -20,25 +29,35 @@ const Formulario = ({pacientes,setPacientes}) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    //Validacion de Formulario
     if ([nombre, propietario, email, fecha, sintomas].includes('')) {
       setError(true);
       return;
     }
     setError(false);
-    const objetoPaciente = {
-      nombre, 
-      propietario, 
-      email, 
-      fecha,
-      sintomas,
-      id: generarId()
-    };
-   
-    setPacientes([
-      ...pacientes,
-      objetoPaciente
-    ]);
 
+    //Objeto de Paciente
+    const objetoPaciente = {
+      nombre,
+      propietario,
+      email,
+      fecha,
+      sintomas
+    };
+    if (paciente.id) {
+      //Editando Registro
+      objetoPaciente.id = paciente.id;
+      const pacientesActualizados = pacientes.map( pacienteState => pacienteState.id === paciente.id ? objetoPaciente : pacienteState );
+      setPacientes(pacientesActualizados);
+      setPaciente({});
+    }
+    else {
+        //Nuevo registro
+        objetoPaciente.id = generarId();
+        setPacientes([...pacientes, objetoPaciente]);
+    }
+    
+    //Reiniciar al Form
     setNombre('');
     setPropietario('');
     setEmail('');
@@ -57,7 +76,7 @@ const Formulario = ({pacientes,setPacientes}) => {
         onSubmit={handleSubmit}
         className="bg-white shadow-md rounded-lg py-10 px-5 mb-10 mx'5">
         {error && <Error><p>Todos los campos son obligatorios</p></Error>
-         }
+        }
         <div className="mb-5">
           <label htmlFor="mascota" className="block text-gray-700 uppercase font-bold">Nombre Mascota</label>
           <input
@@ -106,7 +125,7 @@ const Formulario = ({pacientes,setPacientes}) => {
         </div>
 
         <div className="mb-5">
-          <label htmlFor="sintomas" className="block text-gray-700 uppercase font-bold">Alta</label>
+          <label htmlFor="sintomas" className="block text-gray-700 uppercase font-bold">Sintomas</label>
           <textarea
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
             id="sintomas"
@@ -119,7 +138,7 @@ const Formulario = ({pacientes,setPacientes}) => {
         <input
           type="submit"
           className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-all"
-          value="Agregar Paciente"
+          value={paciente.id ? 'Editar Paciente' : 'Agregar Paciente'}
         />
       </form>
     </div>
